0677673a6eddf684b7f50ef13c64aa87f0260ac0
Michael Nnaji <mnnaji3@uwo.ca>
2020-07-17 23:41:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,12 +9,9 @@ class Triangle
 
   def kind
 
-    if self.side1+self.side3<self.side2 ||  self.side1+self.side2<self.side3 || self.side2 + self.side3<self.side1
+    if self.side1+self.side3<self.side2 ||  self.side1+self.side2<self.side3 || self.side2 + self.side3<self.side1 ||self.side1==0 || self.side3==0 || self.side3==0 || self.side1<0 || self.side2<0 ||  self.side3<0
       raise TriangleError
     end
-    if self.side1==0 || self.side3==0 || self.side3==0
-      raise TriangleError
-     end
 
     if self.side1<0 || self.side2<0 ||  self.side3<0
       raise TriangleError
